50c0e177637602b51f126caf74943d3a7947399f
Cecilie <cecilie@brandt.net>
2021-02-06 14:10:27 +0100

    autoformat

--- 0._Misc/1._Intro_Exercises/2.Strings.js
+++ 0._Misc/1._Intro_Exercises/2.Strings.js
@@ -1,53 +1,53 @@
 // --------------------------------------
 // Exercise 3 - Add numbers from string to float
 
-const numberOne = '1.10';
-const numberTwo = '2.30';
+const numberOne = '1.10'
+const numberTwo = '2.30'
 
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
-const result = Number(numberOne) + Number(numberTwo);
-console.log(result);
+const result = Number(numberOne) + Number(numberTwo)
+console.log(result)
 
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
 
-const anotherNumberOne = '1.10';
-const anotherNumberTwo = '2.30';
+const anotherNumberOne = '1.10'
+const anotherNumberTwo = '2.30'
 
-const total = Number(anotherNumberOne) + Number(anotherNumberTwo);
-console.log(total.toFixed(2));
+const total = Number(anotherNumberOne) + Number(anotherNumberTwo)
+console.log(total.toFixed(2))
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
 
-const one = 10;
-const two = 45;
-const three = 98;
+const one = 10
+const two = 45
+const three = 98
 
 // Show in the console the avg. with 5 decimals
 
-const average = (one + two + three) / 3;
-console.log(average.toFixed(5));
+const average = (one + two + three) / 3
+console.log(average.toFixed(5))
 
 // --------------------------------------
 // Exercise 6 - Get the character by index
 
-const letters = 'abc';
+const letters = 'abc'
 // Get me the character "c"
 
-const index = letters[2];
-console.log(index);
+const index = letters[2]
+console.log(index)
 
 // --------------------------------------
 // Exercise 7 - Replace
 
-const fact = 'You are learning javascript!';
+const fact = 'You are learning javascript!'
 
 // capitalize the J in Javascript
 
-const capitalized2 = fact.replace('j', 'J');
+const capitalized2 = fact.replace('j', 'J')
 
-console.log(capitalized2);
+console.log(capitalized2)
 // --------------------------------------
--- 0._Misc/1._Intro_Exercises/3.Objects.js
+++ 0._Misc/1._Intro_Exercises/3.Objects.js
@@ -3,11 +3,11 @@
 // --------------------------------------
 // Exercise 1 - Retrieve value from object by key
 
-const myObj = { message: 'Hello, earthling! I bring peace.' };
+const myObj = { message: 'Hello, earthling! I bring peace.' }
 
 // Log the message
 
-console.log(myObj.message);
+console.log(myObj.message)
 
 // --------------------------------------
 // Exercise 2 - Defining an object.
@@ -17,30 +17,30 @@ console.log(myObj.message);
 const me = {
   name: 'Cecilie',
   age: 26,
-};
+}
 // --------------------------------------
 // Exercise 3 - Add a property
 
-const stackOverflow = {};
+const stackOverflow = {}
 
 // make a rule called isAllowed and let the value be true
 
-stackOverflow.isAllowed = true;
+stackOverflow.isAllowed = true
 
-console.log(stackOverflow);
+console.log(stackOverflow)
 
 // --------------------------------------
 
 // Exercise 4 - Remove a property
 
-const thisSong = { description: 'The best song in the world.' };
+const thisSong = { description: 'The best song in the world.' }
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute."
 
-delete thisSong.description;
+delete thisSong.description
 
-thisSong.about = 'Just a tribute.';
+thisSong.about = 'Just a tribute.'
 
-console.log(thisSong);
+console.log(thisSong)
 
 // --------------------------------------
