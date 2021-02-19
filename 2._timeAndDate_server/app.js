8a6df809f8a237f8b0051d0f1b9e2635e022b87e
Cecilie <cecilie@brandt.net>
2021-02-19 10:59:24 +0100

    second server assignment done

--- 2._timeAndDate_server/app.js
+++ 2._timeAndDate_server/app.js
@@ -1,4 +1,37 @@
 const express = require('express');
 const app = express();
 
-app.listen()
+const HOST = 'localhost';
+const PORT = 5555;
+
+app.get('/', (req, res) => {
+    res.send({
+        status: res.statusCode,
+        endpoints: {
+            time: `${HOST}:${PORT}/time`,
+            date: `${HOST}:${PORT}/day`,
+            month: `${HOST}:${PORT}/month`,
+        },
+    });
+});
+
+app.get('/time', (req, res) => {
+    const time = new Date().toLocaleTimeString();
+    console.log(time);
+    res.send(`The time is now: ${time}`);
+});
+
+app.get('/day', (req, res) => {
+    const day = new Date().getDay();
+    console.log(day);
+    res.send(`The day is now: ${day}`);
+});
+
+app.get('/month', (req, res) => {
+    const month = new Date().toLocaleDateString('default', { month: 'long' });
+    console.log(month);
+    res.send(`The month is now: ${month}`);
+});
+
+app.listen(PORT);
+console.log(`Server is running on port: ${PORT}`);
