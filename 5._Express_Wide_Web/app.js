f72e891a5cf28800c6cb008d6c2f5370462c80cc
Cecilie <cecilie@brandt.net>
2021-04-07 15:19:25 +0200

    added route /proxy implemented node-fetch for google.com

--- 5._Express_Wide_Web/app.js
+++ 5._Express_Wide_Web/app.js
@@ -30,6 +30,16 @@ app.get('/candle', (req, res) => {
     }
 })
 
+app.get('/proxy', async (req, res) => {
+    try {
+        const response = await fetch('https://google.com/')
+        const body = await response.text()
+        return res.send(body)
+    } catch (error) {
+        res.send({ error: error })
+    }
+})
+
 const server = app.listen(port, (error) => {
     if (error) throw error
     else console.log('Server listening on port:', server.address().port)
